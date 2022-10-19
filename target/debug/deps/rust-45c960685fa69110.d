0f53af253ebbd8418c9ed0946a14a31f7e1e9a60
matheusfrancisco <matheusmachadoufsc@gmail.com>
2022-10-18 21:37:00 -0300

    move

--- target/debug/deps/rust-45c960685fa69110.d
+++
@@ -1,5 +0,0 @@
-/home/matheusfrancisco/m/rust-codes-studies/target/debug/deps/rust-45c960685fa69110.rmeta: main.rs
-
-/home/matheusfrancisco/m/rust-codes-studies/target/debug/deps/rust-45c960685fa69110.d: main.rs
-
-main.rs:
--- target/debug/deps/rust-87b83db1ed759c59.d
+++
@@ -1,5 +0,0 @@
-/home/matheusfrancisco/m/rust-codes-studies/target/debug/deps/rust-87b83db1ed759c59.rmeta: main.rs
-
-/home/matheusfrancisco/m/rust-codes-studies/target/debug/deps/rust-87b83db1ed759c59.d: main.rs
-
-main.rs:
